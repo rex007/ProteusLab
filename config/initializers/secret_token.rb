98e2a4d5f6f88c0c54661749fc650255b3a08ddd
RaDIrkA <radirka@radirka-1225C.(none)>
2014-02-21 15:10:33 +0100

    ProteusLab

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProteusLab::Application.config.secret_key_base = '78b57712be22206a86dcab93e8aab18a3c83431840f7b19a32d1815abd33facf95d62c5d6d2b1dcb611fc541b8c4dc79b850cc6ed252a391c882abf5d3d760a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProteusLab::Application.config.session_store :cookie_store, key: '_ProteusLab_session'
